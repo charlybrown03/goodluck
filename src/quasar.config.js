fa25d57ffb2c890c8e742a4f0331c622cdcae5f9
Carlos Moreno Villodre <carlos.moreno.villodre@gmail.com>
2021-11-24 00:47:03 +0100

    Remove unused font

--- src/quasar.config.js
+++ src/quasar.config.js
@@ -1,10 +1,8 @@
-import '@quasar/extras/roboto-font/roboto-font.css'
-
 import iconSet from 'quasar/icon-set/svg-mdi-v6'
 import lang from 'quasar/lang/en-US'
 
 export default {
   iconSet,
   lang,
-  extras: ['mdi-v6', 'roboto-font'],
+  extras: ['mdi-v6'],
 }
